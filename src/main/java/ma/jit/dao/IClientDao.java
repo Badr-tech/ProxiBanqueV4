50ca79c7260bcbac4de32297e5bfa0fab172a434
Badr <baatouti.pro@gmail.com>
2019-10-16 21:30:53 +0100

    Modification WebService REST

--- src/main/java/ma/jit/dao/IClientDao.java
+++ src/main/java/ma/jit/dao/IClientDao.java
@@ -1,10 +1,16 @@
 package ma.jit.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ma.jit.domaine.Client;
 @RepositoryRestResource
+@CrossOrigin
 public interface IClientDao extends JpaRepository<Client, Long>{
+	
+	public List<Client> findByNom(String nom);
 
 }
